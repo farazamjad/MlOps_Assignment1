3f1c4659ae180e584eea3fb8c14dac6acfc30c42
AsadMinhas782 <86686592+AsadMinhas782@users.noreply.github.com>
2023-02-24 21:12:10 +0500

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ from optim_utils import nn_project
 def sample_input():
     return torch.tensor([[0.1, 0.2], [-0.3, -0.4]])
 
-def nn_project(sample_input):
+def test_nn_project(sample_input):
     expected_output = torch.tensor([[0.4, 0.8], [0.0, 0.0]])
     output = nn_project(sample_input)
     assert torch.allclose(output, expected_output)
