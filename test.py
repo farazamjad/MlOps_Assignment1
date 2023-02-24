8e3a743393bda6c073cb16ddfca43cca829e6f80
AsadMinhas782 <86686592+AsadMinhas782@users.noreply.github.com>
2023-02-24 21:46:06 +0500

    Update test.py

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ from optim_utils import nn_project
 def sample_input():
     return torch.tensor([[0.1, 0.2], [-0.3, -0.4]])
 
-def nn_project(nn_indices: Tensor, embedding_layer: nn.Linear) -> Tensor:
+def test_nn_project(nn_indices: Tensor, embedding_layer: nn.Linear) -> Tensor:
     # Cast nn_indices to the same dtype as embedding_layer.weight
     nn_indices = nn_indices.to(embedding_layer.weight.dtype)
 
