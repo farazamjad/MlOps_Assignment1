626bc36860c448333e521700f74695c38f2356f8
AsadMinhas782 <86686592+AsadMinhas782@users.noreply.github.com>
2023-02-24 21:39:19 +0500

    Update test.py

--- test.py
+++ test.py
@@ -8,9 +8,15 @@ from optim_utils import nn_project
 def sample_input():
     return torch.tensor([[0.1, 0.2], [-0.3, -0.4]])
 
-def test_nn_project(sample_input):
-    embedding_layer = nn.Linear(2, 2)
-    expected_output = torch.tensor([[0.4, 0.8], [0.0, 0.0]])
-    dtype = embedding_layer.weight.dtype
-    output = nn_project(sample_input.unsqueeze(0).to(dtype), embedding_layer)
-    assert torch.allclose(output, expected_output)
+def nn_project(nn_indices: Tensor, embedding_layer: nn.Linear) -> Tensor:
+    # Cast nn_indices to the same dtype as embedding_layer.weight
+    nn_indices = nn_indices.to(embedding_layer.weight.dtype)
+
+    # Project the indices onto the embedding space
+    projected_embeds = embedding_layer(nn_indices)
+
+    # Normalize the projected embeddings
+    norm = projected_embeds.norm(p=2, dim=1, keepdim=True)
+    normalized_embeds = projected_embeds.div(norm)
+
+    return normalized_embeds
