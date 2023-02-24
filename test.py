5591518cde119d09bcae6b38ea332b05299b2a03
AsadMinhas782 <86686592+AsadMinhas782@users.noreply.github.com>
2023-02-24 20:40:45 +0500

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import torch
-from transformers import AutoTokenizer
-
-def test_decode_ids():
-    tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
-    input_ids = torch.tensor([[101, 2023, 2003, 1037, 999, 102], [101, 2023, 2003, 1037, 2000, 102]])
-    decoded = decode_ids(input_ids, tokenizer)
-    assert decoded == ['[CLS] the quick brown [UNK] [SEP]', '[CLS] the quick brown fox [SEP]']
